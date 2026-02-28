16962c354353cfee0f607c8920c741a49d1e0ef5
Deepak <deepakramani65@gmail.com>
2026-02-28 16:16:02 +0530

    resume upload and analysis done

--- src/module/resume/resume.module.ts
+++ src/module/resume/resume.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ResumeService } from './resume.service';
 import { ResumeController } from './resume.controller';
+import { AiModule } from '../ai/ai.module';
 
 @Module({
+  imports: [AiModule],
   providers: [ResumeService],
-  controllers: [ResumeController]
+  controllers: [ResumeController],
 })
 export class ResumeModule {}

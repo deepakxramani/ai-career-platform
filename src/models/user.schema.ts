650140839eb33c254a2b42cf7283a2b5daf780e4
Deepak <deepakramani65@gmail.com>
2026-04-01 21:54:05 +0530

    firebase integration

--- src/models/user.schema.ts
+++ src/models/user.schema.ts
@@ -1,15 +1,18 @@
-import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 
 @Schema()
 export class User {
-    @Prop({ required: true })
-    name: string;
+  @Prop({ required: true })
+  email: string;
 
-    @Prop({ required: true })
-    email: string;
+  @Prop()
+  name: string;
 
-    @Prop({ required: true })
-    password: string;
+  @Prop()
+  password?: string;
+
+  @Prop({ default: 'local' })
+  provider: string;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
